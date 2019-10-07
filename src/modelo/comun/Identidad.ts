e29e73cdc6c9dd22a2c4a3076ee8863ab7109809
Wottan <ivannsuchan@gmail.com>
2019-10-07 08:53:00 -0300

    prueba de typedoc(javadoc)

--- src/modelo/comun/Identidad.ts
+++ src/modelo/comun/Identidad.ts
@@ -1,5 +1,9 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
+/**
+ * Esta clase abstracta se utiliza para que hereden sus propiedades en comun
+ * Autor Ivan Nicolas
+ */
 export abstract class Identidad {
 
     @PrimaryGeneratedColumn()
