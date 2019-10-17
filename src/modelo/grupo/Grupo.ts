5abce6f3fd906224e5777135105cf99968b7c8d1
EliasMoura <d.eliasmoura@hotmail.com>
2019-10-17 11:02:58 -0300

    se agrego el extends Identidad que falto en la clase Grupo, falta la relacion OneToOne en la clase Usuario

--- src/modelo/grupo/Grupo.ts
+++ src/modelo/grupo/Grupo.ts
@@ -1,7 +1,8 @@
 import { Entity, Column } from "typeorm";
+import { Identidad } from "../comun/Identidad";
 
 @Entity()
-export class Grupo {
+export class Grupo extends Identidad{
     @Column()
     nombre: string;
 }
